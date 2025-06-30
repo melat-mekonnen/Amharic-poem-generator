096e6b9f9e9b0a151988ac2e7e8b878c5dbf559f
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2025-06-30 12:23:10 -0700

    adding basic javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("welcome");
+function generatePoem(event) {
+    event.preventDefault();
+    new Typewriter("#poem", {
+        strings: " ሰው አፈር ነው አሉ – ታጥቦ የማይጠራ",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+    
+}
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
