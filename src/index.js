25d194f40293d1450e8d8b7efe77454caa7a90e5
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2025-07-01 04:09:15 -0700

    new change made

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function generatePoem(event) {
     let instructionElement = document.querySelector("#user-instructions");
     let apiKey = "tbf19bo66f32a03f4f87730a62c26921";
     let prompt = `user instutions: Generate a Amharic poem about ${instructionElement.value}`;
-    let context = "you are good love poem writer and you love to connet it with nature and you mission is to generate 4 line poem using html . make sure to follow user instructions. I want to see only the poem part not other word, no html word to show in it";
+    let context = "you are good love poem writer and you love to connet it with nature and you mission is to generate 4 line poem using html . make sure to follow user instructions.";
     console.log(`prompt ${prompt}` )
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios.get(apiUrl).then(displaypoem);
