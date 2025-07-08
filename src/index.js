ff067d6d8d7870d22a94895a9f598b376fb14d74
melat-mekonnen <127474013+melat-mekonnen@users.noreply.github.com>
2025-07-07 22:29:23 -0700

    some little change on the the javascript and html

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
     poemElement.innerHTML = `<div class="generating">⌛ Generating Amharic poem about ${instructionElement.value}</div>`;
     let apiKey = "tbf19bo66f32a03f4f87730a62c26921";
     let prompt = `user instutions: Generate a Amharic poem about ${instructionElement.value}`;
-    let context = "you are good love poem writer and you love to connet it with nature and you mission is to generate 4 line poem using html . make sure to follow user instructions i want to stop the writting after it do once.I want only the poem with no other explanation and it should have poem title with stanza now add this at the end <strong> SheCodes AI</strong>.";
+    let context = "you are good love poem writer and you love to connet it with nature and you mission is to generate 4 line poem using basic html and separate each lire using<br> . make sure to follow user instructions i want to stop the writting after it do once.I want only the poem with no html word and also no other explanation and it should have poem title with stanza now add this at the end <strong> SheCodes AI</strong>.";
     console.log(`prompt ${prompt}` )
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
    
